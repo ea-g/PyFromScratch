23cf2a3b81f9f4ef1f12892f4ad4b65126119df5
ea-g <58582043+ea-g@users.noreply.github.com>
2022-10-22 13:16:40 +0900

    feat: lesson 6 skeleton, lesson 5 solution

---
+++ solutions/lesson_5/lists.py
@@ -0,0 +1,82 @@
+from typing import List, Union
+
+#################################################
+# SETUP
+#################################################
+
+# other lists we'll use
+pizzas_small = [4, 7, 12, 40, 3, 88, 60]
+slices_small = [[12, 5], [0.5, 80], [3, 30], [1000, 0.1], [13, 13]]
+names_small = ['Leo', 'Pikachu', 'Maria', 'Ironman', 'Dan Carter']
+
+#####################################################################################################################
+
+
+def sum_a_list(x: List[Union[int, float]]) -> Union[float, int]:
+    """
+    Sums up all the elements in a given list
+
+    :param x:
+        list with all numeric elements
+    :return:
+        the total of the input list, float or integer
+    """
+    out = 0
+    for i in x:
+        out += i
+    return out
+
+
+def mean_of_list(x: List[Union[int, float]]) -> Union[int, float]:
+    """
+    Finds the mean of a list of numeric items
+    
+    :param x:
+        a list of ints or floats
+    :return:
+        the mean of the input list (int, float)
+    """
+    # fill in the calculation for mean here!
+    return sum_a_list(x)/len(x)
+
+
+def min_of_list(x: List[Union[int, float]]) -> Union[int, float]:
+    """
+    Finds the minimum value of a list of numeric items and returns it.
+    
+    :param x:
+        a list of ints or floats
+    :return:
+        the minimum of the input list
+    """
+    # fill in the method for getting the minimum here!
+    output = None
+    for xi in x:
+        if not output:
+            output = xi
+        else:
+            if xi < output:
+                output = xi
+    return output
+
+
+def max_of_list(x: List[Union[int, float]]) -> Union[int, float]:
+    """
+    Finds the maximum value of a list of numeric items and returns it.
+
+    :param x:
+        a list of ints or floats
+    :return:
+        the maximum of the input list
+    """
+    output = None
+    for xi in x:
+        if not output:
+            output = xi
+        else:
+            if xi > output:
+                output = xi
+    return output
+
+
+
