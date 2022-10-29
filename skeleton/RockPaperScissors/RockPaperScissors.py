7f680d27a1180947aea0be0e81253b84d66d80e6
ea-g <58582043+ea-g@users.noreply.github.com>
2022-10-29 13:14:57 +0900

    feat: RockPaperScissors skeleton

---
+++ skeleton/RockPaperScissors/RockPaperScissors.py
@@ -0,0 +1,13 @@
+from random import choice
+
+
+class RockPaperScissors:
+    def __init__(self, wins: int = 0, ties: int = 0, losses: int = 0, record: list[tuple[str, str]] = None):
+        self.wins = wins
+        self.ties = ties
+        self.loses = losses
+        self.record = record
+
+    def shoot(self):
+        # implement the basic game here!
+        pass
