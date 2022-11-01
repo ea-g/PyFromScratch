86d11294f0717a84306bbd85d95698e93a9c4cfa
ea-g <58582043+ea-g@users.noreply.github.com>
2022-11-01 10:56:51 +0900

    chore: RockPaperScissors skeleton spelling edit

--- skeleton/RockPaperScissors/RockPaperScissors.py
+++ skeleton/RockPaperScissors/RockPaperScissors.py
@@ -5,7 +5,7 @@ class RockPaperScissors:
     def __init__(self, wins: int = 0, ties: int = 0, losses: int = 0, record: list[tuple[str, str]] = None):
         self.wins = wins
         self.ties = ties
-        self.loses = losses
+        self.losses = losses
         self.record = record
 
     def shoot(self):
