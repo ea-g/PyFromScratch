3d58b31a0f414c94a17e424bde747bbef3e4b233
ea-g <58582043+ea-g@users.noreply.github.com>
2023-05-13 13:39:53 +0900

    temp: temporary files for student, will remove

---
+++ skeleton/lesson_23_review/for_leo/review_funcs.py
@@ -0,0 +1,73 @@
+"""
+Review usages for basic data types, relative frequency/probability
+"""
+import re
+import requests
+from bs4 import BeautifulSoup
+
+
+def largest(arr: list[str | int | float]) -> str | int | float:
+    """
+    Looks through a list and returns the largest item. If the list contains strings, uses the length of the string as
+    its size.
+
+    :param arr: a list containing any mix of strings, ints, or floats
+    :return: the largest item in the list
+    """
+    # TODO: Implement the function.
+    out = arr[0]
+    for i in arr:
+        if type(i) is str:
+            if out < len(i):
+                out = i
+        else:
+            if out < i:
+                out = i
+    return out
+
+
+def prob(arr: list[str | int | float], query: str | int | float) -> float:
+    """
+    Calculates the probability of randomly selecting a given item, `query`, given a set of items, `arr`.
+
+    :param arr: the input set of items
+    :param query: the item to randomly select
+    :return: probability of randomly selecting the query item from the set, arr.
+    """
+    # TODO: implement the function. Hint--remember for a discrete set, relative frequency will give us probability
+    overall = len(arr)
+    freq = 0
+    for i in arr:
+        if i == query:
+            freq += 1
+    return freq/overall
+
+
+def get_text(url: str) -> list[str]:
+    """
+    Retrieves all text from paragraph elements from a given url, normalizes it, then splits it into a list of individual
+    words.
+
+    :param url: the target webpage
+    :return: a list of words from the paragraphs on the webpage
+    """
+    # TODO:
+    #  1) get all the the paragraph elements
+    #  2) extract the text from the paragraph elements and make a giant string, all_text
+    #  3) normalize the text using the normalize() function (already made)
+    #  4) split the text into a list of words
+
+    # gets the page content as a BeautifulSoup object
+    page = BeautifulSoup(requests.get(url).content, "html.parser")
+
+    all_text = None
+    pass
+
+
+def normalize(text: str) -> str:
+    return re.sub(r'[^\w\s]', ' ', text.lower())
+
+
+
+
+
