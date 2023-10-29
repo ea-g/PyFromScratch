8effdc8c6ae9bdd3022ef0b8bb091ed0bb31bbc5
ea-g <58582043+ea-g@users.noreply.github.com>
2023-10-28 20:30:06 -0500

    feat: lesson 27 search

---
+++ skeleton/lesson_27/search.py
@@ -0,0 +1,22 @@
+def linear_search(x: list | tuple, target) -> int|None:
+    """
+    Searches through the input array for the target and returns the position if found,
+    otherwise returns none.
+
+    :param x: input array to search through
+    :param target: item to find
+    :return: position of the item in the array if present
+    """
+    pass
+
+
+def binary_search(x: list | tuple, target) -> int|None:
+    """
+    Searches through the input array for the target and returns the position if found,
+    otherwise returns none. Does so using the binary search algorithm (fast)!
+
+    :param x: input array to search through
+    :param target: item to find
+    :return: position of the item in the array if present
+    """
+    pass
