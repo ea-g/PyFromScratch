f1e32a830050716162dcf0fd310b2df5a736e46f
ea-g <58582043+ea-g@users.noreply.github.com>
2024-02-11 11:25:09 +0900

    for time

--- skeleton/flood_fill/FloodFill.py
+++ skeleton/flood_fill/FloodFill.py
@@ -5,6 +5,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 img = Image.open("mario.jpg")
+pik = Image.open("pikachu.jpg")
+pikm = np.array(pik)
+ss = Image.open("shapes.png")
+ssm = np.array(ss)
 mat = np.array(img)
 
 
@@ -35,12 +39,28 @@ class FloodFillBFS:
         show_mat(self.new_color.reshape((1, 1, 3)), "new color")
 
     def is_valid(self, coord):
-        pass
+        sz = self.input_matrix.shape
+        if (coord[0] < 0) or (coord[1] < 0) or (coord[0] >= sz[0]) or (coord[1] >= sz[1]):
+            return False
+        if (self.input_matrix[coord[0], coord[1]] != self.input_matrix[self.start[0], self.start[1]]).any():
+            return False
+        return True
 
     def flood_fill(self, step: int = 0, debug_step: Optional[int] = None):
-        # for debug!
-        step += 1
-        if debug_step:
-            if not step % debug_step:
-                show_mat(self.output_matrix, f"Matrix at step {step}")
-        pass
+        while self.queue:
+            cur = self.queue.pop()
+            up = (cur[0] - 1, cur[1])
+            down = (cur[0] + 1, cur[1])
+            left = (cur[0], cur[1] - 1)
+            right = (cur[0], cur[1] + 1)
+            for dir in [up, down, left, right]:
+                if self.is_valid(dir):
+                    self.output_matrix[dir[0], dir[1], :] = self.new_color
+                    self.queue.appendleft(dir)
+            # for debug! 
+            step += 1
+            if debug_step:
+                if not step % debug_step:
+                    show_mat(self.output_matrix, f"Matrix at step {step}")
+        
+print(1)
