cd5820feff1418cc5f2f45836b12b3d0e41842d6
ea-g <ericgoldberg4@gmail.com>
2024-09-29 08:39:06 -0500

    feat(leo_app_feats): add in circle selection

--- skeleton/image_app/app_progress.py
+++ skeleton/image_app/app_progress.py
@@ -1,7 +1,11 @@
 from nicegui import events, ui
 from pathlib import Path
+from starlette.formparsers import MultiPartParser
 import random
+import base64
 
+
+MultiPartParser.max_file_size = 1024 * 1024 * 5
 # change css style
 ui.add_head_html(
     "<style>" + open(Path(__file__).parent / "styles.css").read() + "</style>"
@@ -10,6 +14,9 @@ ui.add_head_html(
 # address of an image
 src = "https://picsum.photos/id/565/640/360"
 
+def handle_upload(e: events.UploadEventArguments) -> None:
+    b64_bytes = base64.b64encode(e.content.read())
+    ii.set_source(f'data:{e.type};base64,{b64_bytes.decode()}')
 
 # mouse event handler--note that it's used for our interactive image
 def mouse_handler(e: events.MouseEventArguments):
@@ -55,11 +62,54 @@ def draw_rect(e: events.MouseEventArguments):
         del content_len
     # ui.notify(f"{e.type} at ({e.image_x:.1f}, {e.image_y:.1f})")
 
+def clear_image():
+    ii.content = ii.content[:1]
+    
+
+def draw_circle(e: events.MouseEventArguments):
+    global down_coord
+    if e.type=="mousedown":
+        down_coord = (e.image_x, e.image_y)
+        ii.content += f'<circle cx="{down_coord[0]}" cy="{down_coord[1]}" r="{circle_size}" fill="none" stroke-width="4" stroke="black" />'
+
+def switch_circle():
+    ii.on_mouse(draw_circle)
+    if len(ii._event_listeners) > 1:
+        key_1 = list(ii._event_listeners.keys())[0]
+        del ii._event_listeners[key_1]
+        
+def switch_rect():
+    ii.on_mouse(draw_rect)
+    if len(ii._event_listeners) > 1:
+        key_1 = list(ii._event_listeners.keys())[0]
+        del ii._event_listeners[key_1]
+        
+global circle_size
+circle_size = 50  
+def circle_smaller():
+    global circle_size
+    circle_size -= 2
+    # ui.notify(f"circle size is now {circle_size}")
+    zz.set_text(f"{circle_size}")
+    
+def circle_larger():
+    global circle_size
+    circle_size += 2
+    # ui.notify(f"circle size is now {circle_size}")
+    zz.set_text(f"{circle_size}")
 
 with ui.column().classes("w-full items-center"):
+    ui.upload(on_upload=handle_upload).props('accept=.jpg,.jpeg,.png').classes('max-w-full')
     ui.markdown("# Try clicking on this image:")
+    with ui.row():
+        ui.button('Draw Rectangle', on_click=switch_rect)
+        ui.button('Draw Circle', on_click=switch_circle)
+        ui.button('-', on_click=circle_smaller)
+        zz = ui.label(f'{circle_size}')
+        ui.button('+', on_click=circle_larger)
+        ui.button('Clear', on_click=clear_image)
     ii = ui.interactive_image(
-        src, on_mouse=draw_rect, events=["mousedown", "mouseup", "mousemove"], cross=True
+        on_mouse=draw_rect, events=["mousedown", "mouseup", "mousemove"], cross=True
     )
 
 ui.run()
