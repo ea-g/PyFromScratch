e19f7f1eae58990ea80196bf15bfcc8dd9ae23d3
ea-g <ericgoldberg4@gmail.com>
2025-03-22 07:37:48 -0500

    feat: mms floodfill progress contd

--- skeleton/MicroMouse/mms_helpers.py
+++ skeleton/MicroMouse/mms_helpers.py
@@ -1,5 +1,5 @@
 from typing import Literal
-from skeleton.MicroMouse import API
+import API
 from random import choice
 import numpy as np
 from collections import deque
@@ -113,22 +113,27 @@ class FloodFillMMS:
         self.vwalls = np.zeros((size, size - 1))
         self.size = size
 
-    def calc_dist(self):
-        # TODO: reset distances when recalculating
-        # TODO: change the center coords to be based off of self.size
+    def calc_dist(self, mode: Literal['normal', 'return']='normal'):
+        # setup a queue
         q = deque()
-        self.distances[self.size//2 - 1:self.size//2 + 1, self.size//2 - 1:self.size//2 + 1] = 0
-        q.appendleft((self.size//2 - 1, self.size//2 - 1))
-        q.appendleft((self.size//2 - 1, self.size//2))
-        q.appendleft((self.size//2, self.size//2 - 1))
-        q.appendleft((self.size//2, self.size//2))
+        # reset distances when recalculating
+        self.distances = np.ones((self.size, self.size)) * -1
+        if mode=='normal':
+            self.distances[self.size//2 - 1:self.size//2 + 1, self.size//2 - 1:self.size//2 + 1] = 0
+            # add the center coords to queue
+            q.appendleft((self.size//2 - 1, self.size//2 - 1))
+            q.appendleft((self.size//2 - 1, self.size//2))
+            q.appendleft((self.size//2, self.size//2 - 1))
+            q.appendleft((self.size//2, self.size//2))
+        else:
+            # TODO: set the goal to 0 and add it to the queue for returning to the start
+            raise NotImplementedError
         while q:
             cur = q.pop()
             up_down = [(cur[0] - 1, cur[1]), (cur[0] + 1, cur[1])]
             left_right = [(cur[0], cur[1] - 1), (cur[0], cur[1] + 1)]
-            # TODO: check to see if each neighbor is empty  and accessible
-            # TODO: add edge case checks (are neighbors valid/in bounds)
-            # TODO: fix issue with cells blelow and to the right
+            # check to see if each neighbor is empty  and accessible
+            # add edge case checks (are neighbors valid/in bounds)
             # checking and changing up and down
             if self.in_bounds(up_down[0]):
                 if (not self.hwalls[cur[0]-1, cur[1]]) and (self.distances[up_down[0][0], up_down[0][1]] < 0):
@@ -150,10 +155,34 @@ class FloodFillMMS:
                     q.appendleft(left_right[1])
         
     # TODO: make a function to check if a cell is in bounds
-    def in_bounds(self, coord: tuple) -> bool:
+    def in_bounds(self, coord: tuple, mat: Literal['dist', 'hwalls', 'vwalls']='dist') -> bool:
         coord = np.array(coord)
-        return np.all((coord >= 0)) and np.all((coord < self.size))
-    
+        if mat == 'dist':
+            return np.all((coord >= 0)) and np.all((coord < self.size))
+        elif mat == 'hwalls':
+            return np.all((coord >= 0)) and (coord[0] < self.size - 1) and (coord[1] < self.size)
+        else:
+            return np.all((coord >= 0)) and (coord[0] < self.size) and (coord[1] < self.size - 1)
+
+    # TODO: finish the function checking whether a neighbor from 'cur' in direction 'heading' is accessible
+    def is_accessible(self, coord: tuple, heading: Literal['N', 'S', 'W', 'E']) -> bool:
+        """_summary_
+
+        Args:
+            coord (tuple): a given location in the distance matrix
+            heading (str): direction from the coord to check accessibility 
+
+        Returns:
+            bool: whether the cell in a given heading is accessible from coord
+        """
+        if heading == 'N':
+            return not self.hwalls[coord[0]-1, coord[1]]
+        elif heading == 'S':
+            pass
+        elif heading == 'W':
+            pass
+        else:
+            pass
     
     # TODO: define a function to update walls
     def update_walls(self, current_location: tuple, ort: Orientation) -> None:
@@ -161,36 +190,102 @@ class FloodFillMMS:
         # change mms coordinate to matrix style
         pos = mmspos_to_mat(current_location, self.size)
         if ort.disp()=="N":
-            walls['left'] = {'wall': self.vwalls, 'coord': (pos[0], pos[1]-1) if self.in_bounds((pos[0], pos[1]-1)) else None} # note that we check if the wall is in bounds, TODO: double check in bounds part
-            walls['right'] = {'wall': self.vwalls, 'coord': (pos[0], pos[1]) if self.in_bounds((pos[0], pos[1])) else None}
-            walls['front'] = {'wall': self.hwalls, 'coord': (pos[0]-1, pos[1]) if self.in_bounds((pos[0]-1, pos[1])) else None}
-            walls['back'] = {'wall': self.hwalls, 'coord': (pos[0], pos[1]) if self.in_bounds((pos[0], pos[1])) else None}
+            walls['left'] = {'wall': self.vwalls, 'coord': (pos[0], pos[1]-1) if self.in_bounds((pos[0], pos[1]-1), 'vwalls') else None} # note that we check if the wall is in bounds, TODO: double check in bounds part
+            walls['right'] = {'wall': self.vwalls, 'coord': (pos[0], pos[1]) if self.in_bounds((pos[0], pos[1]), 'vwalls') else None}
+            walls['front'] = {'wall': self.hwalls, 'coord': (pos[0]-1, pos[1]) if self.in_bounds((pos[0]-1, pos[1]), 'hwalls') else None}
+            walls['back'] = {'wall': self.hwalls, 'coord': (pos[0], pos[1]) if self.in_bounds((pos[0], pos[1]), 'hwalls') else None}
+            dir_to_heading = {
+                'left': 'w', 
+                'right': 'e',
+                'front': 'n',
+                'back': 's'
+            }
         elif ort.disp()=="S":
             # TODO: fill in S, W, and E versions of which walls are left, right, front, and back
-            pass 
+            walls['left'] = {'wall': self.vwalls, 'coord': (pos[0], pos[1]) if self.in_bounds((pos[0], pos[1]), 'vwalls') else None}
+            walls['right'] = {'wall': self.vwalls, 'coord': (pos[0], pos[1]-1) if self.in_bounds((pos[0], pos[1]-1), 'vwalls') else None}
+            walls['front'] = {'wall': self.hwalls, 'coord': (pos[0], pos[1]) if self.in_bounds((pos[0], pos[1]), 'hwalls') else None}
+            walls['back'] = {'wall': self.hwalls, 'coord': (pos[0]-1, pos[1]) if self.in_bounds((pos[0]-1, pos[1]), 'hwalls') else None}
+            dir_to_heading = {
+                'left': 'e', 
+                'right': 'w',
+                'front': 's',
+                'back': 'n'
+            }
+        elif ort.disp()=="E":
+            walls['back'] = {'wall': self.vwalls, 'coord': (pos[0], pos[1]-1) if self.in_bounds((pos[0], pos[1]-1), 'vwalls') else None} #
+            walls['front'] = {'wall': self.vwalls, 'coord': (pos[0], pos[1]) if self.in_bounds((pos[0], pos[1]), 'vwalls') else None}
+            walls['left'] = {'wall': self.hwalls, 'coord': (pos[0]-1, pos[1]) if self.in_bounds((pos[0]-1, pos[1]), 'hwalls') else None}
+            walls['right'] = {'wall': self.hwalls, 'coord': (pos[0], pos[1]) if self.in_bounds((pos[0], pos[1]), 'hwalls') else None}
+            dir_to_heading = {
+                'left': 'n', 
+                'right': 's',
+                'front': 'e',
+                'back': 'w'
+            }
         else:
-            pass
+            walls['front'] = {'wall': self.vwalls, 'coord': (pos[0], pos[1]-1) if self.in_bounds((pos[0], pos[1]-1), 'vwalls') else None}
+            walls['back'] = {'wall': self.vwalls, 'coord': (pos[0], pos[1]) if self.in_bounds((pos[0], pos[1]), 'vwalls') else None}
+            walls['right'] = {'wall': self.hwalls, 'coord': (pos[0]-1, pos[1]) if self.in_bounds((pos[0]-1, pos[1]), 'hwalls') else None}
+            walls['left'] = {'wall': self.hwalls, 'coord': (pos[0], pos[1]) if self.in_bounds((pos[0], pos[1]), 'hwalls') else None}
+            dir_to_heading = {
+                'left': 's', 
+                'right': 'n',
+                'front': 'w',
+                'back': 'e'
+            }
         # TODO: change the value of the wall from 0 to 1 if there is a wall there
-        for w, val in zip(walls.keys(), [API.wallLeft, API.wallRight, API.wallFront, API.wallBack]):
-            if val():
+        for w, val in zip(sorted(walls.keys()), [API.wallBack, API.wallFront, API.wallLeft, API.wallRight]):
+            if val() and walls[w]['coord']:
+                # make pointers to the correct wall matrix and coordinate 
                 wall = walls[w]['wall']
                 coord = walls[w]['coord']
+                # set each discovered wall to 1 in their matrix
+                wall[coord[0], coord[1]] = 1
+                # show the wall as discovered on the simulator
+                API.setWall(current_location[0], current_location[1], dir_to_heading[w])
                 
+                                
         
     
     # TODO: define a function to determine where to turn based on distance matrix, current location, and orientation
     # will need to get goal and possibly recalculate distance with floodfill
-    def prep_move(self, current_location: tuple, ort: Orientation) -> None:
+    def prep_move(self, current_location: tuple, ort: Orientation, mode: Literal["normal", "return"]) -> None:
         pos = mmspos_to_mat(current_location, self.size)
         # Steps: 
-        # 1) get the distances of neighboring cells
-        # 2) determine which is the best move (lowest) 
-        # 3) check if accessible: 
-        #   if not: recalc dist and start over
-        #   else: turn towards that cell so moving forward will put you in it
+        # 1) get accessible and lower dist neighbors
+        # 2) if none, calc dist and repeat from (1)
+        # 3) turn toward the lowest distance neighbor   
+        redo = True
+        # prepare each neighbor and its corresponding heading
+        headings = ["N", "W", "S", "E"]
+        neighbors = [
+            (pos[0] - 1, pos[1]), 
+            (pos[0], pos[1]-1), 
+            (pos[0] + 1, pos[1]), 
+            (pos[0], pos[1] + 1)
+            ]
+        while redo:
+            # TODO: perform step 1--find which neigbors are accessible and have a distance value lower 
+            # than the current position's distance value. 
+            # If these conditions are met, add the neighbor's distance and heading direction to a list  
+            current_distance = None # TODO: fill me in
+            n_dists = []
+            n_heads = []
+            for h, n in zip(headings, neighbors):
+                if self.in_bounds(n):
+                    neighb_dist = None # TODO: fill me in with the neighbor's distanct (n)
+                    # TODO: finish up step 1
+            if n_heads:
+                redo=False
+            else:
+                # TODO: implement step 2 here
+                pass
+        # TODO: implement step 3--turn toward the lowest distance neighbor
+        # get the heading of the lowest distance neighbor
+        target_heading = n_heads[np.argmin(n_dists)]
+        # TODO: figure out the number of turns it would take to reorient from 
+        # the current orientation to the target heading 
+        # based on the number of turns required, turn the mouse in a direction (back: 'B', left: 'L', or right:'R')
+        
         pass
-
-# ffo = FloodFillMMS()
-# print(ffo.in_bounds((0, 3)))
-# print(ffo.in_bounds((-1, 10)))
-# print(ffo.in_bounds((20, 1)))
