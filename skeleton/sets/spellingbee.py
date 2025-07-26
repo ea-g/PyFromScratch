d63f3c6de4f44db252205ef4aa1c926e990f1709
ea-g <ericgoldberg4@gmail.com>
2025-07-26 08:26:46 -0500

    feat: json intro

---
+++ skeleton/sets/spellingbee.py
@@ -0,0 +1,8 @@
+from collections import defaultdict
+import json
+from os import PathLike
+
+def sets_to_words(words_json_fp: PathLike) -> dict:
+#     make a function to take a word, convert it to a set, add the set to the set dict if cardinality <= 7 and not present, add word to the values 
+#      exclude words with less than 4 letters 
+    pass
